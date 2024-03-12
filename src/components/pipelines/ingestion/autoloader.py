677dbef0132e3ba1b5086750741227d16c4681f2
Olivier Sackhouse <oliviersac@gmail.com>
2024-03-12 17:36:17 -0400

    testing streaming with the start method

--- src/components/pipelines/ingestion/autoloader.py
+++ src/components/pipelines/ingestion/autoloader.py
@@ -43,14 +43,18 @@ class Autoloader:
             .select("*", col("_metadata.file_path").alias("source_file"), current_timestamp().alias("processing_time")))
 
         # Setting stats for the import
-        """
+    
         # AnalysisException: Queries with streaming sources must be executed with writeStream.start(); 
         self.rows_received = streamingDF.count()
         self.rows_added = self.rows_received
-        """
+        
 
         # Can we merge the data instead of just writing
-        (streamingDF.writeStream
-            .options(**writestream_options)  
-            .trigger(availableNow=True)
-            .toTable(f"{destination_table_name}", mode="append"))
+        query = (streamingDF.writeStream
+                .options(**writestream_options)
+                .trigger(availableNow=True)
+                .toTable(f"{destination_table_name}", mode="append")
+                .start())  # Start the streaming query
+
+        # Wait for the termination of the query
+        query.awaitTermination()
