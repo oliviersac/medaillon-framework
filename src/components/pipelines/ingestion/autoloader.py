fddf170182657483983f77416b636ff8cacae465
Olivier Sackhouse <oliviersac@gmail.com>
2024-03-12 17:45:58 -0400

    removing spaces

--- src/components/pipelines/ingestion/autoloader.py
+++ src/components/pipelines/ingestion/autoloader.py
@@ -52,25 +52,3 @@ class Autoloader:
             .options(**writestream_options)  
             .trigger(availableNow=True)
             .toTable(f"{destination_table_name}", mode="append"))
-
-
-        """
-        # Obtaining data from spark readstream
-        streamingDF = (spark.readStream
-            .format("cloudFiles")
-            .options(**cloudfile_options)
-            .schema(spark_schema)
-            .load(file_path)
-            .select("*", col("_metadata.file_path").alias("source_file"), current_timestamp().alias("processing_time")))
-        # Setting stats for the import
-
-        # AnalysisException: Queries with streaming sources must be executed with writeStream.start(); 
-        #self.rows_received = streamingDF.count()
-        #self.rows_added = self.rows_received
-
-        # Can we merge the data instead of just writing
-        (streamingDF.writeStream
-            .options(**writestream_options)  
-            .trigger(availableNow=True)
-            .toTable(f"{destination_table_name}", mode="append"))
-        """
