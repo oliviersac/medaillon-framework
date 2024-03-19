ec373cbdeb4a5e5fb8690fd63a4cb626ff9dd176
Olivier Sackhouse <oliviersac@gmail.com>
2024-03-19 01:33:24 -0400

    added stats for ingestion

--- src/components/jobs/ingest.py
+++ src/components/jobs/ingest.py
@@ -6,6 +6,7 @@ import importlib
 from pipelines.ingestion.autoloader import Autoloader
 from handlers.parameters_handler.argument_parser import ArgumentParser
 from writers.transfer_log_writer import TransferLogWriter
+from readers.delta_source_reader import DeltaReader
 
 
 def main(parameters):
@@ -20,15 +21,24 @@ def main(parameters):
     module = importlib.import_module(schema_path)
     spark_schema = module.TopicSchema.getSchema()
 
+    # Counting rows before autoload
+    table_count_origin = DeltaReader.loadTableCount(destination_table_name)
+
     # Doing autoload
     autoloader = Autoloader(spark_schema)
     autoloader.autoload_to_table(spark,file_path,destination_table_name,checkpoint_path)
 
+     # Counting rows after autoload and setting the value for the log table
+    table_count_end = DeltaReader.loadTableCount(destination_table_name)
+    rows_received = table_count_end - table_count_origin
+    rows_added = rows_received
+
+
     # Insert a new transfer log entry
     log_writer = TransferLogWriter(spark)
     log_writer.writeTransferLog('S3', 'S3-dev-landing', 'delta', destination_table_name, '', 
-                                autoloader.rows_received, autoloader.rows_filtered, 
-                                autoloader.rows_deduped, autoloader.rows_added, 'SUCCESS', '')
+                                rows_received, autoloader.rows_filtered, 
+                                autoloader.rows_deduped, rows_added, 'SUCCESS', '')
 
 if __name__ == '__main__':
   parameters = ArgumentParser.parse_arguments(sys.argv)
