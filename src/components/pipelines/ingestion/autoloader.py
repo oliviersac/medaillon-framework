eb455e73f993901a73b25ef7e8c2e231e6461b43
Olivier Sackhouse <oliviersac@gmail.com>
2024-03-19 20:23:22 -0400

    lets try something

--- src/components/pipelines/ingestion/autoloader.py
+++ src/components/pipelines/ingestion/autoloader.py
@@ -12,6 +12,7 @@ class Autoloader:
         self.spark_schema = spark_schema
         self.destination_table_name = destination_table_name
         self.write_mode = "append"
+        self.query = None
     
     def process_batch(self, batch_df, batch_id):
         # Count the number of rows received in the current batch
@@ -24,7 +25,7 @@ class Autoloader:
         batch_df.write.mode(self.write_mode).saveAsTable(self.destination_table_name)
 
 
-    def autoload_to_table(self, spark, file_path: StringType, destination_table_name: StringType, checkpoint_path: StringType):
+    def autoload_to_table(self, spark, file_path: StringType,destination_table_name: StringType,checkpoint_path: StringType):
         # Set the schema for the data to stream
         spark_schema = self.spark_schema
 
@@ -37,8 +38,14 @@ class Autoloader:
             "cloudFiles.schemaLocation": checkpoint_path,
             "cloudfiles.useNotifications": "true",
             "cloudFiles.schemaEvolutionMode": "rescue",
-            "cloudFiles.maxFilesPerTrigger": 2000,
+            "cloudFiles.maxFilesPerTrigger": 5000,
             "cloudFiles.maxBytesPerTrigger": "10g"
+        }
+
+        # Defining the write stream options for saving the data
+        writestream_options = {
+            "checkpointLocation" : checkpoint_path,
+            "mergeSchema": "true" # Merging the new schema with the existing one automatically 
         }
 
         # Obtaining data from spark readstream
@@ -49,13 +56,12 @@ class Autoloader:
             .load(file_path)
             .select("*", col("_metadata.file_path").alias("source_file"), current_timestamp().alias("processing_time")))
         
-        # Apply processing to each batch of the streaming dataframe
-        query = (streamingDF
-                 .writeStream
-                 .foreachBatch(self.process_batch)
-                 .outputMode("append")
-                 .option("checkpointLocation", checkpoint_path)
+        # Start the streaming query
+        query = (streamingDF.writeStream
+                 .options(**writestream_options)  
+                 .trigger(availableNow=True)
+                 .toTable(f"{destination_table_name}", mode="append")
                  .start())
         
-        # Await termination
-        query.awaitTermination()
+        # Store the query object
+        self.query = query
